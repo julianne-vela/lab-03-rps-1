0bbe1e667ee9be3f7c16412d1ca0a8cab54a18e5
Nicole Martin <n.martinpdx@gmail.com>
2021-01-13 17:15:06 -0800

    add import export js files getting close!

--- app.js
+++ app.js
@@ -1,50 +1,66 @@
+import { getRandomThrow } from '../getRandomThrow.js';
+import { doesUserWin } from './doesUserWin.js';
+
 // import functions and grab DOM elements
 const goButton = document.getElementById('go-button');
-const currentPick = document.getElementById('current-pick');
+// const currentPick = document.getElementById('current-pick');
 const winSpan = document.getElementById('wins');
 const loseSpan = document.getElementById('losses');
-const resultSpan = document.getElementById('result');
+const drawSpan = document.getElementById('draws');
+const totalSpan = document.getElementById('total-clicks');
 
 // console.log(goButton, currentPick, winSpan, loseSpan, result);
 
 // initialize state
 let wins = 0;
-let result = 0;
+let lose = 0;
+let draw = 0;
+let total = 0;
 
 // set event listeners to update state and DOM
 
 goButton.addEventListener('click', () => {
-    result++;
-
-    const compChoice = Math.round(Math.random() * 3);
-    // console.log(compChoice);
-    const compRPS = makeRPS(compChoice);
+    total++;
 
     const selectedButton = document.querySelector(`input[type='radio']:checked`);
 
+    const compChoice = getRandomThrow();
     const userChoice = selectedButton.value;
 
-    if (userChoice === compRPS) {
-        wins++;
-        currentPick.textContent = `You guessed ${userChoice}. The correct guess was ${compChoice}`;
-    }
 
-    function makeRPS(oneOrTwoThree) {
-        if (oneOrTwoThree === 1) {
-            return 'Rock';
-        }
-  
-        else (oneOrTwoThree === 2); {
-            return 'Paper';
-        }
-  
-  
+    const theWinner = doesUserWin(userChoice, compChoice);
+
+    if (theWinner === 'win') {
+        wins++;
+    }
+    else if (theWinner === 'lose') {
+        lose++;
+    }
+    else if (theWinner === 'draw') {
+        draw++;
     }
 
     winSpan.textContent = wins;
-    loseSpan.textContent = result - wins;
-    resultSpan.textContent = result;
+    loseSpan.textContent = lose;
+    drawSpan.textContent = draw;
+    totalSpan.textContent = total;
+    
 });
+  
 
 
 
+    // if (userChoice === compRPS) {
+    //     wins++;
+    //     currentPick.textContent = ` Rad!! You guessed ${userChoice}. The correct guess was ${compChoice}`;
+    // } else {
+    //     currentPick.textContent = `Nope!! You guessed ${userChoice}. The correct guess was ${compChoice}`;
+    // }
+
+    
+
+// function makeRPS(oneOrTwoThree) {
+//     if (oneOrTwoThree === 1) return 'rock'; 
+//     if (oneOrTwoThree === 2) return 'paper';
+//     if (oneOrTwoThree === 3) return 'scissors';
+// }
---
+++ doesUserWin.js
@@ -0,0 +1,10 @@
+export function doesUserWin(player, computer) {
+    if (player === computer) return 'draw';
+    if (player === 'rock' && computer === 'scissors') return 'win';
+    if (player === 'rock' && computer === 'paper') return 'lose';
+    if (player === 'scissors' && computer === 'paper') return 'win';
+    if (player === 'scissors' && computer === 'rock') return 'lose';
+    if (player === 'paper' && computer === 'rock') return 'win';
+    if (player === 'paper' && computer === 'scissors') return 'lose';
+    
+}

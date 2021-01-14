36439c5b09ea98e7766ce8d659ffd3ef15f2a6c0
Julianne Vela <cjvela@outlook.com>
2021-01-14 15:48:16 -0800

    refacted getRandomThrow to ternary operator

--- getRandomThrow.js
+++ getRandomThrow.js
@@ -1,7 +1,5 @@
 export function getRandomThrow(compThrow) {
-    if (compThrow === 1) {
-        return 'rock';
-    } else if (compThrow === 2) {
-        return 'paper';
-    } return 'scissors';
+    return compThrow === 1 ? 'rock'
+        : compThrow === 2 ? 'paper'
+            : 'scissors';
 }

2f10d9f27228a92c22f1b72f5618d228b2ee526d
Julianne Vela <cjvela@outlook.com>
2021-01-14 15:41:04 -0800

    refactored getRandomThrow function

--- getRandomThrow.js
+++ getRandomThrow.js
@@ -1,15 +1,7 @@
-export function getRandomThrow(compThrow){
-    // const compChoice = Math.round(Math.random() * 3);
-  
+export function getRandomThrow(compThrow) {
     if (compThrow === 1) {
         return 'rock';
-    }
-    if (compThrow === 2) {
+    } else if (compThrow === 2) {
         return 'paper';
-    }
-    if (compThrow === 3) {
-        return 'scissors';
-    }
-    
+    } return 'scissors';
 }
-

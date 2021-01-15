89622c96e76e59206b4b2ebb6da6a09a430eaf4e
Julianne Vela <cjvela@outlook.com>
2021-01-14 16:58:53 -0800

    refactored tests and created tests for doesUserWin function

--- test/example.test.js
+++ test/example.test.js
@@ -1,46 +1,41 @@
-// IMPORT MODULES under test here:
-import { getRandomThrow } from '../getRandomThrow.js';
+import {
+    getRandomThrow,
+    doesUserWin
+} from '../function-utils.js';
 
 const test = QUnit.test;
 
-test('it should return rock when given 1 as the argument', (expect) => {
-    //Arrange
-    // Set up your arguments and expectations
+test('it should return rock when given 1, paper when given 2, and scissors when given 3 as the argument', (expect) => {
     const expected = 'rock';
-    
-    //Act 
-    // Call the function you're testing and set the result to a const
+    const expected2 = 'paper';
+    const expected3 = 'scissors';
+
     const actual = getRandomThrow(1);
+    const actual2 = getRandomThrow(2);
+    const actual3 = getRandomThrow(3);
 
-    //Expect
-    // Make assertions about what is expected versus the actual result
     expect.equal(actual, expected);
+    expect.equal(actual2, expected2);
+    expect.equal(actual3, expected3);
 });
 
-test('it should return paper when given 2 as the argument', (expect) => {
-    //Arrange
-    // Set up your arguments and expectations
-    const expected = 'paper';
-    
-    //Act 
-    // Call the function you're testing and set the result to a const
-    const actual = getRandomThrow(2);
-
-    //Expect
-    // Make assertions about what is expected versus the actual result
-    expect.equal(actual, expected);
+// DRAW TESTS
+test('Result should be draw when player and computer throws are the same', (assert) => {
+    assert.strictEqual(doesUserWin('rock', 'rock'), 'draw', ['Results should be draw']);
+    assert.strictEqual(doesUserWin('paper', 'paper'), 'draw', ['Results should be draw']);
+    assert.strictEqual(doesUserWin('scissors', 'scissors'), 'draw', ['Results should be draw']);
 });
 
-test('it should return scissors when given 3 as the argument', (expect) => {
-    //Arrange
-    // Set up your arguments and expectations
-    const expected = 'scissors';
-    
-    //Act 
-    // Call the function you're testing and set the result to a const
-    const actual = getRandomThrow(3);
+// LOSE TESTS
+test('Result should be lose when p=rock & c=paper or p=paper & c=scissors or p=scissors & c=rock', (assert) => {
+    assert.strictEqual(doesUserWin('rock', 'paper'), 'lose', ['Results should be lose']);
+    assert.strictEqual(doesUserWin('paper', 'scissors'), 'lose', ['Results should be lose']);
+    assert.strictEqual(doesUserWin('scissors', 'rock'), 'lose', ['Results should be lose']);
+});
 
-    //Expect
-    // Make assertions about what is expected versus the actual result
-    expect.equal(actual, expected);
+// WIN TESTS
+test('Result should be win when player throws rock, paper, and scissors while computer throws scissors, rock, and paper', (assert) => {
+    assert.strictEqual(doesUserWin('rock', 'scissors'), 'win', ['Results should be win']);
+    assert.strictEqual(doesUserWin('paper', 'rock'), 'win', ['Results should be win']);
+    assert.strictEqual(doesUserWin('scissors', 'paper'), 'win', ['Results should be win']);
 });
